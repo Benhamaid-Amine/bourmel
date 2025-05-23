18c0141fc3bfdae7fc29ec75b6f8e24749d521d9
Benhamaid-Amine <aminocheminou@gmail.com>
2025-05-23 18:47:25 +0100

    v1.0.5

--- app/algorithms/[name]/page.js
+++ app/algorithms/[name]/page.js
@@ -10,85 +10,99 @@ const algorithms = {
     "SubstitutionSimple": {
       link: "https://www.programiz.com/online-compiler/6K0wG36g81ln7",
       description: "Le chiffrement par substitution consiste à remplacer chaque lettre du texte clair par une autre lettre selon une permutation fixe de l'alphabet. Le déchiffrement utilise l'inverse de cette permutation.",
+      Download: "https://drive.google.com/uc?export=download&id=1mIx3Ikx5GDnXVFlJp2-fNRq2CHO9Difz"
     },
     "Cesar": {
       link: "https://www.programiz.com/online-compiler/2sFLtHXiTIf9K",
       description: "Le chiffre de César consiste à décaler chaque lettre d’un nombre fixe de positions dans l’alphabet. Par exemple, avec un décalage de 3, A devient D, B devient E, etc.",
       Chiffrement: "C(M) = (M + K ) mod 26",
       Dechiffrement: "D(C) = (C - K ) mod 26", 
+      Download: "https://drive.google.com/uc?export=download&id=12F5D4TxRS16AX_0Cispr7EgRSzEvn3ze"
     },
     "Albam": {
       link: "https://www.programiz.com/online-compiler/0YuJHY6XltVjR",
       description: "Le chiffrement Albam (ou ROT13) est un chiffrement par substitution simple qui consiste à remplacer chaque lettre par celle située 13 positions plus loin dans l’alphabet. Il est symétrique et involutif, c’est-à-dire que la même opération sert à chiffrer et déchiffrer.Mathématiquement, on associe chaque lettre à une valeur de 0 à 25 (A=0, B=1, ..., Z=25). ",
       Chiffrement: "C(M)=(M+13)mod26",
       Dechiffrement: "D(C)=(C-13)mod26", 
+      Download: "https://drive.google.com/uc?export=download&id=1eGCHVxFinPvKmHeOdpMHZFxkQQucPFIz"
     },
     "Atbash": {
     link: "https://www.programiz.com/online-compiler/765ZKPJJ6oA2a",
     description: "Le chiffrement Atbash est une méthode de substitution dans laquelle chaque lettre de l'alphabet est remplacée par sa lettre opposée (A ↔ Z, B ↔ Y, C ↔ X, etc.).",
     Chiffrement: "C(M) = 25 - M",
-    Dechiffrement: "D(C) = 25 - C"
+    Dechiffrement: "D(C) = 25 - C",
+    Download: "https://drive.google.com/uc?export=download&id=17uaTrJa9U0IZXqfbiJ0r_aHrth7tcL29"
   },
   
     "Atbah": {
       link: "https://www.programiz.com/online-compiler/7ABHpQpSVs2Co",
       description: "Atbush est une autre appellation ou variante historique d’Atbash, utilisée dans certains contextes religieux ou ésotériques.",
       Chiffrement: "C(M) = 25 - M",
-      Dechiffrement: "D(C) = 25 - C"
+      Dechiffrement: "D(C) = 25 - C",
+      Download: "https://drive.google.com/uc?export=download&id=1d05e1X1BCMAqJGJfjUC61jhcHs_BmFaO"
     },
     "Alberti": {
       link: "https://www.programiz.com/online-compiler/0gfeWBa3RuCag",
       description: "Premier chiffrement polyalphabétique utilisant un système de disques rotatifs, inventé par Leon Battista Alberti en 1467.il a Une clé (lettre) pour le décalage initial.Une rotation périodique du décalage (tous les 3 caractères ici).",
       Chiffrement: "C(M)=(M+shift)mod26 // Shift=(shift+1)mod26 ",
       Dechiffrement: "D(C)=(C−shift+26)mod26 ",
+      Download: "https://drive.google.com/uc?export=download&id=1o89sVNhi5nZ3qhf2ZdOTOfNcRCDgYI_f"
     },
     "Polybe": {
       link: "https://www.programiz.com/online-compiler/1lOG6NTfpXjED",
       description: "Le carré de Polybe chiffre chaque lettre par une paire de coordonnées (ligne, colonne) dans une grille 5x5. Le déchiffrement associe chaque paire à une lettre.",
       Chiffrement: "C(M)=(ligne_M,colonne_M)",
       Dechiffrement: "D(C)=caractere en (ligne_C,colonne_C)",
+      Download: "https://drive.google.com/uc?export=download&id=16BdAKWU1TDV0wugMjDeC7asNn3t7r2uK"
     },
     "Trithemius": {
       link: "https://www.programiz.com/online-compiler/0m6RQfGm9zTpU",
       description: "Le chiffre de Trithemius est une variante de Vigenère avec une clé qui évolue automatiquement, souvent de manière arithmétique (ex : ABCDE...).",
       Chiffrement: "Ci=(Mi +i)mod26(ou i commence a 0)",
       Dechiffrement: "D(C)=(Ci - i + 26)mod26",
+      Download: "https://drive.google.com/uc?export=download&id=13PMjjthK9NpblJRWVmaRTwvC3KE8xSWB"
     },
     "Vigenere": {
       link: "https://www.programiz.com/online-compiler/2K0wGCwEQ1kvC",
       description: "Le chiffre de Vigenère est un chiffrement polyalphabétique utilisant une clé répétée pour déterminer le décalage de chaque lettre du message.",
       Chiffrement: "Ci =(Mi + K( i mod len(K) )  )mod26",
       Dechiffrement: "Mi =(Ci - K ( i mod len(K) ) )mod26",
+      Download: "https://drive.google.com/uc?export=download&id=15XZD90D7PlI-uul-UY_-b4sC3cpX4At2"
     },
     "Autokey": {
       link: "https://www.programiz.com/online-compiler/393qLjDbHiP0S",
       description: "Autokey est une extension de Vigenère : la clé commence avec un mot-clé puis continue avec le texte clair lui-même, rendant le chiffrement plus résistant.",
       Chiffrement: "Ci = (Mi + Ki )   mod26",
       Dechiffrement: "Mi =(Ci - Ki + 26 )mod26",
+      Download: "https://drive.google.com/uc?export=download&id=1knXC6V5_bW3U2w5WEjNbKf2YyX4eLkTE"
     },
     "Porta": {
       link: "https://www.programiz.com/online-compiler/41UTNfgS2RwmD",
       description: "Le chiffre de Porta est un système polyalphabétique où chaque lettre de la clé définit une table de substitution spécifique pour encoder le texte.",
       Chiffrement: "C = Porta(M, K)",
       Dechiffrement: "M = Porta^{-1}(C, K)",
+      Download: "https://drive.google.com/uc?export=download&id=1sm5lRo5FBSUyhspcUaSTGXo4yOa2kqDp"
     },
     "Beaufort": {
       link: "https://www.programiz.com/online-compiler/3RZWBq5XSClmW",
       description: "Le chiffre de Beaufort est similaire à Vigenère mais inverse la logique de chiffrement, en soustrayant la lettre de la clé au lieu de l’ajouter.",
       Chiffrement: "C=( K- M ) mod 26",
       Dechiffrement: "D=( K- C ) mod 26",
+      Download: "https://drive.google.com/uc?export=download&id=1hS-ZOsFtWqOh8Ueg2fYoOaAWZkq8kr0g"
     },
     "Vernam(otp)": {
       link: "https://www.programiz.com/online-compiler/3nS1XOvpvmsSA",
       description: "Le chiffre de Vernam (OTP) est un chiffrement parfait utilisant une clé aléatoire aussi longue que le message, chaque caractère étant combiné avec un XOR.",
       Chiffrement: "C = M ⊕ K",
       Dechiffrement: "M = C ⊕ K",
+      Download: "https://drive.google.com/uc?export=download&id=18wvI7YixUly5sQPn7WrDwYv4VA_rPxo3"
     },
       "DES": {
       link: "https://www.programiz.com/online-compiler/5pg7sOnglYeyJ",
       description: "Algorithme de chiffrement symétrique par blocs (64 bits) utilisant une clé de 56 bits et 16 tours de transformations.",
       Chiffrement: "C = DES_Encrypt(M, K)",
       Dechiffrement: "M = DES_Decrypt(C, K)",
+      Download: "https://drive.google.com/uc?export=download&id=1_zZsd7KH2PgmeqA9w_3pLT5Sy3CKgU6L"
     },
     
   }; // 9C 71 A2 C7 3F 7C 42 C8 key: FD 1C CB A9 5A 1E 2C A0
@@ -128,7 +142,7 @@ export default function AlgorithmPage() {
         </div>
       </div>
     ), {
-      duration: 8000,
+      duration: 5000,
       position: 'bottom-right',
     });
   }, []);
@@ -165,6 +179,25 @@ export default function AlgorithmPage() {
                   {algo.Dechiffrement}
                 </code>
               </div>
+              <a
+  href={algo.Download}
+  download
+  target="_blank"
+  rel="noopener noreferrer"
+  className="inline-flex items-center px-6 py-3 bg-black/85 text-white font-semibold rounded-lg shadow-md hover:bg-black/75 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-5 h-5 mr-2"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5m0 0l5-5m-5 5V4" />
+  </svg>
+  Download Code
+</a>
             </div>
 
             <div className="mt-8">
